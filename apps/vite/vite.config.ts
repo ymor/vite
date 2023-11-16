c0ef4de043b8f54f8d72f56c912c5834b45e0b3e
Mark Hebden <mark.hebden@airelogic.com>
2023-11-16 12:54:49 +0000

    Add example commented out reporters

--- apps/vite/vite.config.ts
+++ apps/vite/vite.config.ts
@@ -30,5 +30,6 @@ export default defineConfig({
     },
     environment: 'jsdom',
     include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    //reporters: ['junit', 'basic']
   },
 });
